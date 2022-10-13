a7108a48b96695a36a93f61e5238266a6e3a8eda
JulioCz36 <juliojero05@gmail.com>
2022-10-13 12:03:35 -0300

    agregue nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonEditComponent } from './components/button-edit/button-edit.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArPrComponent } from './components/logo-ar-pr/logo-ar-pr.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LoginComponent } from './components/login/login.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { AddComponent } from './components/add/add.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonEditComponent,
+    HeaderComponent,
+    LogoArPrComponent,
+    SocialComponent,
+    BannerComponent,
+    LoginComponent,
+    AcercaDeComponent,
+    DeleteComponent,
+    AddComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
